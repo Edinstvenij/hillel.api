ff78a6921539efd9937c38c5f21b75226b31e82a
Yura <yuravigovskij@gmail.com>
2022-11-13 21:52:48 +0200

    add migrates

---
+++ database/migrations/2022_11_13_191539_create_label_project_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('label_project', function (Blueprint $table) {
+            $table->foreignId('label_id')->constrained();
+            $table->foreignId('project_id')->constrained();
+
+            $table->primary(['label_id', 'project_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lable_project');
+    }
+};
